3f20d2126571dcbf0aa3bb2dcfeeda17d4ba9d1b
iron6312 <edhardy312312@gmail.com>
2020-12-14 18:34:39 +0900

    写真投稿完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
   def index
+    @posts = Post.includes(:user)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  private
+  def post_params
+    params.require(:post).permit(:title, :description, :recomended, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :description
+    validates :recomended
+    validates :image
+  end
 end
