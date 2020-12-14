0a76bdbb6554ebcc67834f569fb193e2cd26d612
iron6312 <edhardy312312@gmail.com>
2020-12-14 15:56:33 +0900

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :age
+    validates :camp_history
+    validates :favorite_camp_type
+  end
+end
