b7412b2c03e75cdb925bd22c89dc21aa6b204e1b
iron6312 <edhardy312312@gmail.com>
2020-12-16 20:16:20 +0900

    いいね機能まで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   def index
     @posts = Post.includes(:user)
   end
@@ -9,15 +12,51 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = ost.new(post_params)
     if @post.save
       redirect_to root_path
     else
       render :new
     end
   end
+
+  def show
+    @comment = Comment.new
+    @comments = @post.comments
+    @like = Like.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+  
   private
+
   def post_params
     params.require(:post).permit(:title, :description, :recomended, :image).merge(user_id: current_user.id)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @post.user
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :posts
+  has_many :post, dependent: :destroy
+  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
+
+  def already_liked?(post)
+    self.likes.exists?(post_id: post.id)
+  end
 
   with_options presence: true do
     validates :nickname
